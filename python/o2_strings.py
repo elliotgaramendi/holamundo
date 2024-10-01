58cbf53d28718f15fb382fefbddb29d43c40ddbd
elliotgaramendi <elliotgaramendi@gmail.com>
2024-09-30 21:30:49 -0500

    feat: Create python o2_strings.py

---
+++ python/o2_strings.py
@@ -0,0 +1,15 @@
+""" Strings """
+
+LANGUAGE = "Python"
+DESCRIPTION = """
+Python is a programming language
+that lets you work more quickly and
+integrate your systems more effectively.
+"""
+
+print(len(LANGUAGE))
+print(LANGUAGE[4])
+print(LANGUAGE[:2])
+print(LANGUAGE[2:4])
+print(LANGUAGE[4:])
+print(LANGUAGE, DESCRIPTION)
