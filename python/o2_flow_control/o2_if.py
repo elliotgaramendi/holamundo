b8a8ac8fdca1d35e46fabe299193b86189742210
elliotgaramendi <elliotgaramendi@gmail.com>
2024-10-07 10:51:24 -0500

    feat: Create python o2 o2_if.py

---
+++ python/o2_flow_control/o2_if.py
@@ -0,0 +1,12 @@
+""" If """
+
+AGE = 28
+
+if AGE >= 60:
+    print("You have a super discount")
+elif AGE >= 18:
+    print("You don't have a discount")
+else:
+    print("You can't enter")
+
+print("You are a", AGE, "years old")
