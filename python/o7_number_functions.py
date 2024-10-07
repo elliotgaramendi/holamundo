daeb7352b8110bb46fdc21c97fa59a05fb4f97e6
elliotgaramendi <elliotgaramendi@gmail.com>
2024-10-07 09:28:22 -0500

    feat: Create python o7_number_functions.py

---
+++ python/o7_number_functions.py
@@ -0,0 +1,15 @@
+""" Number functions """
+
+import math
+
+print(round(8.2))
+print(round(8.5))
+print(round(8.555, 2))
+print(abs(8))
+print(abs(-8))
+
+print(math.ceil(8.2))
+print(math.floor(8.5))
+print(math.isnan(8.2))
+print(math.pow(8, 2))
+print(math.sqrt(4))
