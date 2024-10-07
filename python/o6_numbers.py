12a8ffae00c2a77fd2ee4cbd77f52741e328e5e3
elliotgaramendi <elliotgaramendi@gmail.com>
2024-10-07 08:44:41 -0500

    feat: Create python o6_numbers.py

---
+++ python/o6_numbers.py
@@ -0,0 +1,18 @@
+# pylint: disable=invalid-name
+
+""" Numbers """
+
+FAVORITE = 8
+special = 8.5
+IMAGINARY = 8j
+
+print(FAVORITE + 5)
+print(FAVORITE - 5)
+print(FAVORITE * 5)
+print(FAVORITE / 5)
+print(FAVORITE // 5)
+print(FAVORITE % 5)
+print(FAVORITE ** 5)
+
+special -= 0.5
+print(special)
