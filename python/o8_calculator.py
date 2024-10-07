a67f0a789ef4195827a4a6c34d33218982bac854
elliotgaramendi <elliotgaramendi@gmail.com>
2024-10-07 09:42:08 -0500

    feat: Create python o8_calculator.py

---
+++ python/o8_calculator.py
@@ -0,0 +1,16 @@
+""" Calculator """
+
+print("Calculator")
+
+number1 = int(input("Enter first number: "))
+number2 = int(input("Enter second number: "))
+
+print(f"""
+  {number1} + {number2} = {number1 + number2}
+  {number1} -  {number2} = {number1 - number2}
+  {number1} *  {number2} = {number1 * number2}
+  {number1} /  {number2} = {number1 / number2}
+  {number1} // {number2} = {number1 // number2}
+  {number1} %  {number2} = {number1 % number2}
+  {number1} ** {number2} = {number1 ** number2}
+""")
