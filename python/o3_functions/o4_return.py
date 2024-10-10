2cdfe6715965589daf85cb99d3f6cea57a97ae80
elliotgaramendi <elliotgaramendi@gmail.com>
2024-10-10 14:46:51 -0500

    feat: Create python o4_return.py

---
+++ python/o3_functions/o4_return.py
@@ -0,0 +1,11 @@
+""" return """
+
+
+def addition(number1, number2):
+    """Return the sum of two numbers."""
+    return number1 + number2
+
+
+RESULT = addition(5, 8)
+print(RESULT)
+print(addition(RESULT, 15))
