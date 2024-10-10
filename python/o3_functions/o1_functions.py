004ac7a1173682f0d42285cd465073352d4d01c6
elliotgaramendi <elliotgaramendi@gmail.com>
2024-10-10 13:11:09 -0500

    feat: Create python o3 o1_functions.py

---
+++ python/o3_functions/o1_functions.py
@@ -0,0 +1,11 @@
+""" Functions """
+
+
+def hello(state, name="user"):
+    """Greets the user with a given state and name."""
+    print(f"Hello and welcome {state} {name}!")
+
+
+hello("Happy", "Chanchirata")
+hello("Smiling", "Elliot")
+hello(state="Sad")
