bab47dd23f1ea9476692c98bf08d22f313aba72b
elliotgaramendi <elliotgaramendi@gmail.com>
2024-10-10 21:20:29 -0500

    feat: Create python o4 o1_lists.py

---
+++ python/o4_advanced_types/o1_lists.py
@@ -0,0 +1,21 @@
+""" Lists """
+
+numbers = [1, 2, 3, 4, 5]
+letters = ['a', 'b', 'c', 'd', 'e']
+words = ['one', 'two', 'three', 'four', 'five']
+names = ['Elliot', 17, True]
+matrix = [[1, 2, 3], [4, 5, 6], [7, 8, 9]]
+favoriteNumber = [8] * 5
+alphanumeric = numbers + letters
+rangeOfNumbers = list(range(1, 9) )
+chars = list("Chanchirata")
+
+print(numbers)
+print(letters)
+print(words)
+print(names)
+print(matrix)
+print(favoriteNumber)
+print(alphanumeric)
+print(rangeOfNumbers)
+print(chars)
