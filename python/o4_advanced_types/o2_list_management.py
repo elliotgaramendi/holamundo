1d2be41012d5e8fbc1b42dbeff149f977c4f5557
elliotgaramendi <elliotgaramendi@gmail.com>
2024-10-18 08:46:53 -0500

    feat: Create python o4 o2_list_management.py

---
+++ python/o4_advanced_types/o2_list_management.py
@@ -0,0 +1,13 @@
+""" List Management """
+
+pets = ["Doky", "Rocky", "Chispa", "NN", "Chocolate"]
+print(pets)
+pets[0] = "Doky 2.0"
+print(pets)
+print(pets[0])
+print(pets[-1])
+print(pets[2:])
+print(pets[::2])
+
+numbers = list(range(16))
+print(numbers[2:15:3])
